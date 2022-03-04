b150a31ca8e72e30ce64631103890a4f3da54adf
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2022-03-04 21:05:25 +0700

    tambah

--- backend/views/penyewa/_search.php
+++ backend/views/penyewa/_search.php
@@ -8,7 +8,7 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="penyewa-search">
+<div class="penyewa-search"> 
 
     <?php $form = ActiveForm::begin([
         'action' => ['index'],
